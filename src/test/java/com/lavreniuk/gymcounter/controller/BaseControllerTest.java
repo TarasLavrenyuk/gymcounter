dab6f9723d1f9ac6afb0d2498bdb4d94665ad972
TarasLavrenyuk <lavrik1997@gmail.com>
2018-04-30 23:53:46 +0300

    Add empty test.

--- src/test/java/com/lavreniuk/gymcounter/controller/BaseControllerTest.java
+++ src/test/java/com/lavreniuk/gymcounter/controller/BaseControllerTest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lavreniuk.gymcounter.domain.User;
 import org.json.JSONObject;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -68,4 +69,8 @@ public class BaseControllerTest {
         return httpHeaders;
     }
 
+    @Test
+    public void name() throws Exception {
+
+    }
 }
